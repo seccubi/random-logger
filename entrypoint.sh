d7b96dd64693620172f9bfa53d34848ea8a127e7
koza4ok <koza4okzak@gmail.com>
2021-03-20 22:34:57 -0500

    Added extra tags and IPs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+UUID=$(head -c100 /dev/urandom | tr -dc 'a-zA-Z0-9')
+tags="[${UUID:0:4}=${UUID:3:4}] [${UUID:7:4}=${UUID:10:4}] [${UUID:13:4}]"
+IP1=$(printf "%d.%d.%d.%d\n" "$((RANDOM % 256))" "$((RANDOM % 256))" "$((RANDOM % 256))" "$((RANDOM % 256))")
+IP2=$(printf "%d.%d.%d.%d\n" "$((RANDOM % 256))" "$((RANDOM % 256))" "$((RANDOM % 256))" "$((RANDOM % 256))")
+EXTRA="$tags $IP1 $IP2"
 n=-1
 c=0
 if [ -n "$3" ]
@@ -12,14 +18,15 @@ do
    sleep $(echo "scale=4; $WAIT/1000" | bc)
    I=$(shuf -i 1-4 -n 1)
    D=`date -Iseconds`
+#   IP=
    case "$I" in
-      "1") echo "$D ERROR An error is usually an exception that has been caught and not handled."
+      "1") echo "$D ERROR An error is usually an exception that has been caught and not handled. $EXTRA"
       ;;
-      "2") echo "$D INFO This is less important than debug log and is often used to provide context in the current task."
+      "2") echo "$D INFO This is less important than debug log and is often used to provide context in the current task. $EXTRA"
       ;;
-      "3") echo "$D WARN A warning that should be ignored is usually at this level and should be actionable."
+      "3") echo "$D WARN A warning that should be ignored is usually at this level and should be actionable. $EXTRA"
       ;;
-      "4") echo "$D DEBUG This is a debug log that shows a log that can be ignored."
+      "4") echo "$D DEBUG This is a debug log that shows a log that can be ignored. $EXTRA"
       ;;
    esac
    c=$(( c+1 ))
